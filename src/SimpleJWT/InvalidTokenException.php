6a91fdbffa8aec2a32e9d720c86a6e564490cd20
Kelvin Mo <kmo@users.sourceforge.net>
2015-07-19 09:21:09 +1000

    Initial implementation of JWT

---
+++ src/SimpleJWT/InvalidTokenException.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ * SimpleJWT
+ *
+ * Copyright (C) Kelvin Mo 2015
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace SimpleJWT;
+
+/**
+ * An exception where a JWT or JWE is invalid or cannot be decoded for any reason.
+ */
+class InvalidTokenException extends \RuntimeException {
+    /** An error code indicating that the JWT or JWE cannot be parsed
+     * (e.g. not a valid JSON object) */
+    const TOKEN_PARSE_ERROR = 0;
+
+    /** An error code indicating that the JWT or JWE contains critical claims
+     * that are not supported by SimpleJWT */
+    const UNSUPPORTED_ERROR = 1;
+
+    /** An error code indicating that the JWT's signature cannot be verified.
+     * This may be due to the lack of a key, cryptographic errors, or the
+     * signature is incorrect. */
+    const SIGNATURE_VERIFICATION_ERROR = 16;
+
+    /** An error code indicating that the JWE cannot be decrypted.
+     * This may be due to the lack of a key, cryptographic errors, or the
+     * authentication information is incorrect. */
+    const DECRYPTION_ERROR = 17;
+
+    /** An error code indicating that the JWT or JWE is invalid as a result
+     * of the `nbf` claim.  The time that the token is valid can be obtained
+     * using the {@link getTime()} function. */
+    const TOO_EARLY_ERROR = 256;
+
+    /** An error code indicating that the JWT or JWE is invalid as a result
+     * of the `exp` claim.  The time that the token was valid until can be obtained
+     * using the {@link getTime()} function. */
+    const TOO_LATE_ERROR = 257;
+
+    protected $time;
+
+    public function __construct($message = "", $code = 0, $previous = NULL, $time = 0) {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getTime() {
+        return $this->time;
+    }
+}
+
+?>
---
+++ src/SimpleJWT/JWT.php
@@ -0,0 +1,248 @@
+<?php
+/*
+ * SimpleJWT
+ *
+ * Copyright (C) Kelvin Mo 2015
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace SimpleJWT;
+
+use SimpleJWT\Crypt\AlgorithmFactory;
+use SimpleJWT\Crypt\CryptException;
+use SimpleJWT\Keys\KeyException;
+use SimpleJWT\Util\Util;
+
+/**
+ * A JSON web token (JWT) with a signature.
+ *
+ * A JWT consists of a header and a set of claims.  To create a JWT, use the
+ * constructor with the header and claims as the parameter.  The JWT can then
+ * be signed and serialised using the {@link encode()} function.
+ *
+ * To decode a serialised JWT, use the {@link decode()} static function.  If
+ * successful, the static function will return a JWT object.  The headers and
+ * claims can then be retrieved using the {@link getHeaders()} and {@link getClaims()}
+ * functions.
+ */
+class JWT {
+    const COMPACT_FORMAT = 0;
+    const JSON_FORMAT = 1;
+
+    static public $TIME_ALLOWANCE = 300;
+
+    protected $headers = array(
+        'typ' => 'JWT',
+        'alg' => 'RS256'
+    );
+
+    protected $claims = array();
+
+    /**
+     * Creates a new JWT.
+     *
+     * @param array $headers the headers
+     * @param array $claims the claims
+     */
+    public function __construct($headers, $claims) {
+        $this->headers = $headers;
+        $this->claims = $claims;
+    }
+
+    /**
+     * Decodes a serialised JWT.
+     *
+     * @param string $token the serialised JWT
+     * @param SimpleJWT\Keys\KeySet $keys the key set containing the key to verify the
+     * JWT's signature
+     * @param string $expected_alg the expected value of the `alg` parameter, which
+     * should be agreed between the parties out-of-band
+     * @param string $kid the ID of the key to use to verify the signature. If null, this
+     * is automatically retrieved
+     * @param int $format the JWT serialisation format
+     * @return JWT the decoded JWT
+     * @throws InvalidTokenException if the token is invalid for any reason
+     */
+    public static function decode($token, $keys, $expected_alg, $kid = null, $format = self::COMPACT_FORMAT) {
+        switch ($format) {
+            case self::COMPACT_FORMAT:
+                list($protected, $payload, $signature) = explode('.', $token, 3);
+                break;
+            case self::JSON_FORMAT:
+                $obj = json_decode($token, true);
+                if ($obj == null) throw new InvalidTokenException('Cannot decode JSON', InvalidTokenException::TOKEN_PARSE_ERROR);
+                $payload = $obj['payload'];
+
+                if (isset($obj['signatures'])) {
+                    foreach ($obj['signatures'] as $signature_obj) {
+                        if (isset($signature_obj['header']['kid'])) {
+                            $target_kid = $signature_obj['header']['kid'];
+                            if (($target_kid == $kid) || ($keys->getById($target_kid) != null)) {
+                                $unprotected = $signature_obj['header'];
+                                $protected = $signature_obj['protected'];
+                                $signature = $signature_obj['signature'];
+                                break;
+                            }
+                        }
+                        throw new InvalidTokenException('Cannot find verifiable signature', InvalidTokenException::TOKEN_PARSE_ERROR);
+                    }
+                } else {
+                    $unprotected = $obj['header'];
+                    $protected = $obj['protected'];
+                    $signature = $obj['signature'];
+                }
+                break;
+            default:
+                throw new \InvalidArgumentException('Incorrect format');
+        }
+
+        $headers = json_decode(Util::base64url_decode($protected), true);
+        if ($headers == null) throw new InvalidTokenException('Cannot decode header', InvalidTokenException::TOKEN_PARSE_ERROR);
+
+        // Process crit
+        if (isset($headers['crit'])) {
+            foreach ($headers['crit'] as $critical) {
+                if (!in_array($critical, array('nbf', 'exp', 'alg', 'kid'))) {
+                    throw new InvalidTokenException('Critical header not supported: ' . $critical, InvalidTokenException::UNSUPPORTED_ERROR);
+                }
+            }
+        }
+
+        if (isset($unprotected)) $headers = array_merge($headers, $unprotected);
+
+        $claims = json_decode(Util::base64url_decode($payload), true);
+        if ($claims == null) throw new InvalidTokenException('Cannot decode claims', InvalidTokenException::TOKEN_PARSE_ERROR);
+
+        // Check signatures
+        if ($headers['alg'] != $expected_alg) throw new InvalidTokenException('Unexpected algorithm', InvalidTokenException::SIGNATURE_VERIFICATION_ERROR);
+        $signer = AlgorithmFactory::create($expected_alg);
+        $signing_input = $protected . '.' . $payload;
+
+        try {
+            $result = $signer->verify($signature, $signing_input, $keys, $kid);
+        } catch (KeyException $e) {
+            throw new InvalidTokenException($e->getMessage(), InvalidTokenException::SIGNATURE_VERIFICATION_ERROR, $e);
+        } catch (CryptException $e) {
+            throw new InvalidTokenException($e->getMessage(), InvalidTokenException::SIGNATURE_VERIFICATION_ERROR, $e);
+        }
+
+        if (!$result) throw new InvalidTokenException('Incorrect signature', InvalidTokenException::SIGNATURE_VERIFICATION_ERROR);
+
+        // Check time, etc
+        if (isset($headers['nbf'])) {
+            if (time() < $headers['nbf'] + self::$TIME_ALLOWANCE) throw new InvalidTokenException('Too early due to nbf header', InvalidTokenException::TOO_EARLY_ERROR, null, $headers['nbf']);
+        }
+
+        if (isset($headers['exp'])) {
+            if (time() < $headers['exp'] - self::$TIME_ALLOWANCE) throw new InvalidTokenException('Too late due to exp header', InvalidTokenException::TOO_LATE_ERROR, null, $headers['exp']);
+        }
+
+        return new JWT($headers, $claims);
+    }
+
+    /**
+     * Returns the JWT's headers.
+     *
+     * @return array the headers
+     */
+    public function getHeaders() {
+        return $this->headers;
+    }
+
+    /**
+     * Returns a specified header
+     *
+     * @param string $header the header to return
+     * @return mixed the header value
+     */
+    public function getHeader($header) {
+        return $this->headers[$header];
+    }
+
+    /**
+     * Returns the JWT's claims.
+     *
+     * @return array the claims
+     */
+    public function getClaims() {
+        return $this->claims;
+    }
+
+    /**
+     * Returns a specified claim
+     *
+     * @param string $claim the claim to return
+     * @return mixed the claim value
+     */
+    public function getClaim($claim) {
+        return $this->claims[$claim];
+    }
+
+    /**
+     * Signs and serialises the JWT.
+     *
+     * @param SimpleJWT\Keys\KeySet $keys the key set containing the key to sign the
+     * JWT
+     * @param string $kid the ID of the key to use to sign. If null, this
+     * is automatically retrieved
+     * @param bool $include_iat if true, override the `iat` header with the current
+     * time
+     * @param string $alg if not null, override the `alg` header
+     * @param int $format the JWT serialisation format
+     * @return string the signed and serialised JWT
+     * @throws SimpleJWT\Keys\KeyException if there is an error obtaining the key
+     * to sign the JWT
+     * @throws SimpleJWT\Crypt\CryptException if there is a cryptographic error
+     */
+    public function encode($keys, $kid = null, $include_iat = true, $alg = null, $format = self::COMPACT_FORMAT) {
+        if ($alg != null) $this->headers['alg'] = $alg;
+        if ($include_iat )$this->headers['iat'] = time();
+
+        $signer = AlgorithmFactory::create($this->headers['alg']);
+        $protected = Util::base64url_encode(json_encode($this->headers));
+        $payload = Util::base64url_encode(json_encode($this->claims));
+        $signing_input = $protected . '.' . $payload;
+        $signature = $signer->sign($signing_input, $keys, $kid);
+
+        switch ($format) {
+            case self::COMPACT_FORMAT:
+                return $signing_input . '.' . $signature;
+            case self::JSON_FORMAT:
+                return json_encode(array(
+                    'protected' => $protected,
+                    'payload' => $payload,
+                    'signature' => $signature
+                ));
+            default:
+                throw new \InvalidArgumentException('Incorrect format');
+        }
+    }
+}
+?>

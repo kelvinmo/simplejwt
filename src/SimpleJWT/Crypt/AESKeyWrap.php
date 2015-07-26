da737b17ab019eff03e5fde404976cc6113adc94
Kelvin Mo <kmo@users.sourceforge.net>
2015-07-26 10:45:20 +1000

    Implementation of AES key wrap

---
+++ src/SimpleJWT/Crypt/AESKeyWrap.php
@@ -0,0 +1,156 @@
+<?php
+/*
+ * SimpleJWT
+ *
+ * Copyright (C) Kelvin Mo 2015
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace SimpleJWT\Crypt;
+
+use SimpleJWT\Util\Util;
+use SimpleJWT\Keys\Key;
+
+/**
+ * Interface for content authenticated encryption algorithms.
+ */
+class AESKeyWrap extends Algorithm implements KeyEncryptionAlgorithm {
+
+    const RFC3394_IV = "\xA6\xA6\xA6\xA6\xA6\xA6\xA6\xA6";
+
+    static protected $alg_params = array(
+        'A128KW' => array('cipher' => 'AES-128-ECB', 'key' => 32),
+        'A192KW' => array('cipher' => 'AES-192-ECB', 'key' => 48),
+        'A256KW' => array('cipher' => 'AES-256-ECB', 'key' => 64),
+    );
+
+    public function __construct($alg) {
+        parent::__construct($alg);
+    }
+
+    public function getSupportedAlgs() {
+        $ciphers = openssl_get_cipher_methods();
+        $results = array();
+
+        foreach (self::$alg_params as $alg => $param) {
+            if (in_array($param['cipher'], $ciphers)) {
+                $results[] = $alg;
+            }
+        }
+
+        return $results;
+    }
+
+    public function getKeyCriteria() {
+        $alg = $this->getAlg();
+        $size = self::$alg_params[$alg]['key'] * 8;
+        return array('kty' => 'oct', Key::SIZE_PROPERTY => $size, '~alg' => $this->getAlg());
+    }
+
+    public function encryptKey($cek, $keys, &$headers, $kid = null) {
+        if ($kid == null) {
+            $key = $this->selectKey($keys);
+        } else {
+            $key = $this->selectKey($keys, array("kid" => $kid));
+        }
+
+        if ((strlen($cek) % 8) != 0) throw new CryptException('Content encryption key not a multiple of 64 bits');
+
+        $cipher = self::$alg_params[$this->getAlg()]['cipher'];
+
+        $A = self::RFC3394_IV;
+        $P = str_split($cek, 8);
+        $R = str_split($cek, 8);
+        $n = count($P);
+
+        for ($j = 0; $j <= 5; $j++) {
+            for ($i = 0; $i < $n; $i++) {
+                $t = $n * $j + ($i + 1);
+                $B = openssl_encrypt($A . $R[$i], $cipher, $key->toBinary(), OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING);
+                $A = $this->msb($B) ^ Util::packInt64($t);
+                $R[$i] = $this->lsb($B);
+            }
+        }
+
+        return Util::base64url_encode($A . implode('', $R));
+    }
+
+    public function decryptKey($encrypted_key, $keys, $headers, $kid = null) {
+        if ($kid == null) {
+            $key = $this->selectKey($keys);
+        } else {
+            $key = $this->selectKey($keys, array("kid" => $kid));
+        }
+
+        $cipher = self::$alg_params[$this->getAlg()]['cipher'];
+
+        $R = str_split(Util::base64url_decode($encrypted_key), 8);
+        $A = array_shift($R);
+        $n = count($R);
+
+        for ($j = 5; $j >= 0; $j--) {
+            for ($i = $n - 1; $i >= 0; $i--) {
+                $t = $n * $j + ($i + 1);
+                $B = openssl_decrypt(($A ^ Util::packInt64($t)) . $R[$i], $cipher, $key->toBinary(), OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING);
+                $A = $this->msb($B);
+                $R[$i] = $this->lsb($B);
+            }
+        }
+
+        if (!Util::secure_compare($A, self::RFC3394_IV)) {
+            throw new CryptException('AES key wrap integrity check failed');
+        }
+
+        return implode('', $R);
+    }
+
+    /**
+     * Returns the most significant half of a specified value
+     *
+     * @param string $x the value
+     * @return string the most significant half
+     */
+    protected function msb($x) {
+        return substr($x, 0, strlen($x) / 2);
+    }
+
+    /**
+     * Returns the least significant half of a specified value
+     *
+     * @param string $x the value
+     * @return string the least significant half
+     */
+    protected function lsb($x) {
+        return substr($x, strlen($x) / 2);
+    }
+
+}
+
+?>
---
+++ tests/AESKeyWrapTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace SimpleJWT\Crypt;
+
+use SimpleJWT\Util\Util;
+
+class AESKeyWrapTest extends \PHPUnit_Framework_TestCase {
+
+    protected function getKeySet($kek) {
+        return \SimpleJWT\Keys\KeySet::createFromSecret($kek, 'bin');
+    }
+
+    protected function hex2base64url($hex) {
+        return Util::base64url_encode(hex2bin($hex));
+    }
+
+    function testA128KWWith128Key() {
+        $kek = hex2bin('000102030405060708090A0B0C0D0E0F');
+        $key = hex2bin('00112233445566778899AABBCCDDEEFF');
+
+        $alg = new AESKeyWrap('A128KW');
+        $set = $this->getKeySet($kek);
+        $headers = array();
+        $encrypted_key = $alg->encryptKey($key, $set, $headers);
+        $this->assertEquals($this->hex2base64url('1FA68B0A8112B447AEF34BD8FB5A7B829D3E862371D2CFE5'), $encrypted_key);
+
+        $decrypted_key = $alg->decryptKey($encrypted_key, $set, $headers);
+        $this->assertEquals($key, $decrypted_key);
+    }
+
+    function testA192KWWith128Key() {
+        $kek = hex2bin('000102030405060708090A0B0C0D0E0F1011121314151617');
+        $key = hex2bin('00112233445566778899AABBCCDDEEFF');
+
+        $alg = new AESKeyWrap('A192KW');
+        $set = $this->getKeySet($kek);
+        $headers = array();
+        $encrypted_key = $alg->encryptKey($key, $set, $headers);
+        $this->assertEquals($this->hex2base64url('96778B25AE6CA435F92B5B97C050AED2468AB8A17AD84E5D'), $encrypted_key);
+
+        $decrypted_key = $alg->decryptKey($encrypted_key, $set, $headers);
+        $this->assertEquals($key, $decrypted_key);
+    }
+
+    function testA256KWWith128Key() {
+        $kek = hex2bin('000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F');
+        $key = hex2bin('00112233445566778899AABBCCDDEEFF');
+
+        $alg = new AESKeyWrap('A256KW');
+        $set = $this->getKeySet($kek);
+        $headers = array();
+        $encrypted_key = $alg->encryptKey($key, $set, $headers);
+        $this->assertEquals($this->hex2base64url('64E8C3F9CE0F5BA263E9777905818A2A93C8191E7D6E8AE7'), $encrypted_key);
+
+        $decrypted_key = $alg->decryptKey($encrypted_key, $set, $headers);
+        $this->assertEquals($key, $decrypted_key);
+    }
+
+    function testA192KWWith192Key() {
+        $kek = hex2bin('000102030405060708090A0B0C0D0E0F1011121314151617');
+        $key = hex2bin('00112233445566778899AABBCCDDEEFF0001020304050607');
+
+        $alg = new AESKeyWrap('A192KW');
+        $set = $this->getKeySet($kek);
+        $headers = array();
+        $encrypted_key = $alg->encryptKey($key, $set, $headers);
+        $this->assertEquals($this->hex2base64url('031D33264E15D33268F24EC260743EDCE1C6C7DDEE725A936BA814915C6762D2'), $encrypted_key);
+
+        $decrypted_key = $alg->decryptKey($encrypted_key, $set, $headers);
+        $this->assertEquals($key, $decrypted_key);
+    }
+
+    function testA256KWWith192Key() {
+        $kek = hex2bin('000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F');
+        $key = hex2bin('00112233445566778899AABBCCDDEEFF0001020304050607');
+
+        $alg = new AESKeyWrap('A256KW');
+        $set = $this->getKeySet($kek);
+        $headers = array();
+        $encrypted_key = $alg->encryptKey($key, $set, $headers);
+        $this->assertEquals($this->hex2base64url('A8F9BC1612C68B3FF6E6F4FBE30E71E4769C8B80A32CB8958CD5D17D6B254DA1'), $encrypted_key);
+
+        $decrypted_key = $alg->decryptKey($encrypted_key, $set, $headers);
+        $this->assertEquals($key, $decrypted_key);
+    }
+
+    function testA256KWWith246Key() {
+        $kek = hex2bin('000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F');
+        $key = hex2bin('00112233445566778899AABBCCDDEEFF000102030405060708090A0B0C0D0E0F');
+
+        $alg = new AESKeyWrap('A256KW');
+        $set = $this->getKeySet($kek);
+        $headers = array();
+        $encrypted_key = $alg->encryptKey($key, $set, $headers);
+        $this->assertEquals($this->hex2base64url('28C9F404C4B810F4CBCCB35CFB87F8263F5786E2D80ED326CBC7F0E71A99F43BFB988B9B7A02DD21'), $encrypted_key);
+
+        $decrypted_key = $alg->decryptKey($encrypted_key, $set, $headers);
+        $this->assertEquals($key, $decrypted_key);
+    }
+}
+?>

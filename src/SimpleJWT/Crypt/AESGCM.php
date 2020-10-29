f70a5f136fd6529dc0c095e0683acdffc9362b94
Kelvin Mo <kelvinmo@users.noreply.github.com>
2020-10-29 23:23:01 +1100

    Fix incorrect encoding for AES GCM decryption (#34)

--- src/SimpleJWT/Crypt/AESGCM.php
+++ src/SimpleJWT/Crypt/AESGCM.php
@@ -107,7 +107,7 @@ class AESGCM extends Algorithm implements EncryptionAlgorithm {
         $tag = Util::base64url_decode($tag);
         if (strlen($tag) != 16) throw new CryptException('Incorrect authentication tag length');
         
-        $plaintext = openssl_decrypt($ciphertext, $params['cipher'], $cek, OPENSSL_RAW_DATA, $iv, $tag, $additional);
+        $plaintext = openssl_decrypt(Util::base64url_decode($ciphertext), $params['cipher'], $cek, OPENSSL_RAW_DATA, $iv, $tag, $additional);
         if ($plaintext === false) throw new CryptException('Authentication tag does not match');
 
         return $plaintext;

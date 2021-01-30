2f87a7086d779d066dced3669cdc639ceda48830
Kelvin Mo <kmo@users.sourceforge.net>
2021-01-30 13:55:16 +1100

    Update copyright

--- src/SimpleJWT/Crypt/AESGCM.php
+++ src/SimpleJWT/Crypt/AESGCM.php
@@ -2,7 +2,7 @@
 /*
  * SimpleJWT
  *
- * Copyright (C) Kelvin Mo 2020
+ * Copyright (C) Kelvin Mo 2020-2021
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions

4c736e2891563b8d157a91bf3517e5ac673f5130
Kelvin Mo <kmo@users.sourceforge.net>
2022-01-26 22:32:42 +1100

    Update documentation for None

--- src/SimpleJWT/Crypt/None.php
+++ src/SimpleJWT/Crypt/None.php
@@ -37,6 +37,18 @@ namespace SimpleJWT\Crypt;
 
 /**
  * Implements the `none` signature algorithm.
+ * 
+ * Note that the `none` algorithm should only be used in JWTs which
+ * are cryptographically protected by other means.
+ * 
+ * By default, the `none` algorithm is disabled in SimpleJWT.  Attempts
+ * to decode a JWT with a `none` algorithm will return a
+ * {@link SimpleJWT\InvalidTokenException}.  In order to enable this
+ * algorithm, call {@link AlgorithmFactory::addNoneAlg()} static
+ * method.
+ * 
+ * @link https://datatracker.ietf.org/doc/html/rfc8725.html#section-3.2
+ * @codeCoverageIgnore
  */
 class None extends Algorithm implements SignatureAlgorithm {
     public function __construct($alg) {

2c367d303c5338b6dffab9e1cf147389843b2eb9
Kelvin Mo <kmo@users.sourceforge.net>
2022-03-12 13:57:20 +1100

    Fix type checking in PEM strings

--- src/SimpleJWT/Keys/KeyFactory.php
+++ src/SimpleJWT/Keys/KeyFactory.php
@@ -109,7 +109,7 @@ class KeyFactory {
 
         // 2. Decode JSON
         if ($format == 'json') {
-            /* @var string $data */
+            /** @var string $data */
             $json = json_decode($data, true);
             if (isset($json['ciphertext'])) {
                 $format = 'jwe';
@@ -151,9 +151,9 @@ class KeyFactory {
             $der = new DER();
 
             if (preg_match(Key::PEM_PUBLIC, $data, $matches)) {
-                /** @var string|bool $binary */
+                /** @var string $binary */
                 $binary = base64_decode($matches[1]);
-                if ($binary === FALSE) throw new KeyException('Cannot read PEM key');
+                if ($binary == FALSE) throw new KeyException('Cannot read PEM key');
 
                 $seq = $der->decode($binary);
 
@@ -161,12 +161,14 @@ class KeyFactory {
 
                 $oid = $seq->getChildAt(0)->getChildAt(0)->getValue();
                 if (isset(self::$oid_map[$oid])) {
-                    return new self::$oid_map[$oid]($data, 'pem');
+                    /** @var Key $key */
+                    $key = new self::$oid_map[$oid]($data, 'pem');
+                    return $key;
                 }
             } elseif (preg_match(Key::PEM_PKCS8_PRIVATE, $data, $matches)) {
-                /** @var string|bool $binary */
+                /** @var string $binary */
                 $binary = base64_decode($matches[1]);
-                if ($binary === FALSE) throw new KeyException('Cannot read PEM key');
+                if ($binary == FALSE) throw new KeyException('Cannot read PEM key');
 
                 $seq = $der->decode($binary);
 

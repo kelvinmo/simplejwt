ef953269fc75f860ae0b79b4980fa6676f17a162
kelvinmo <kelvinmo@users.noreply.github.com>
2024-01-01 03:18:22 +0000

    Update copyright year

--- src/SimpleJWT/Crypt/KeyManagement/ECDH_AESKeyWrap.php
+++ src/SimpleJWT/Crypt/KeyManagement/ECDH_AESKeyWrap.php
@@ -2,7 +2,7 @@
 /*
  * SimpleJWT
  *
- * Copyright (C) Kelvin Mo 2020-2023
+ * Copyright (C) Kelvin Mo 2020-2024
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions

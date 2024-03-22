fec69bad21d5f8ba0b48e3511c2e86463eaf257c
Kelvin Mo <kmo@users.sourceforge.net>
2024-03-22 20:14:42 +1100

    Add release task to RoboFile.php

--- RoboFile.php
+++ RoboFile.php
@@ -1,13 +1,16 @@
 <?php
 
+use Robo\Tasks as RoboTasks;
+use Robo\Symfony\ConsoleIO;
 use Symfony\Component\Finder\Finder;
+use PHLAK\SemVer\Version;
 
 /**
  * This is project's console commands configuration for Robo task runner.
  *
  * @see http://robo.li/
  */
-class RoboFile extends \Robo\Tasks {
+class RoboFile extends RoboTasks {
     protected function checkPharReadonly() {
         if (ini_get('phar.readonly')) {
             throw new \Exception('Must set "phar.readonly = Off" in php.ini to build phars.');
@@ -70,5 +73,91 @@ class RoboFile extends \Robo\Tasks {
                 ->to('Copyright (C) Kelvin Mo $1-'. $current_year . '$2')
                 ->run();
         }
+    }
+
+    /**
+     * Prepares a release.
+     * 
+     * @param string $type one of major, minor, patch, pre-release
+     * @param array $opts
+     * @option $dry-run Dry-run, do not make changes
+     * @option $ignore-worktree Ignore unstaged and uncommitted changes
+     * @option $prefix Prefix to be prepended to version number
+     * @option $update-changelog Update the Changelog
+     * @option $changelog File name to changelog file
+     * @option $push Push git changes
+     */
+    public function release(ConsoleIO $io, $type = 'patch', $opts = [ 'prefix' => 'v', 'dry-run' => false, 'update-changelog' => true, 'changelog' => 'CHANGELOG.md', 'push' => true, 'ignore-worktree' => false ]) {
+        // 1. Check parameters and unstage/uncommitted changes
+        if (!in_array($type, ['major', 'minor', 'patch', 'pre-release'])) {
+            $io->error("type is not one of major, minor, patch, pre-release");
+            return 1;
+        }
+
+        if (!$opts['ignore-worktree']) {
+            $clean_worktree = $this->taskGitStack()
+                ->exec('update-index -q --ignore-submodules --refresh')
+                ->exec('diff-files --quiet --ignore-submodules')
+                ->exec('diff-index --cached --quiet HEAD --ignore-submodules')
+                ->run();
+            if (!$clean_worktree->wasSuccessful()) {
+                $io->error('You have unstaged or uncommitted changes');
+                return $clean_worktree;
+            }
+        }
+
+        // 2. Get the current tag
+        $version_task = $this->taskExec('git describe --tags --abbrev=0 HEAD')->run();
+        if (!$version_task->wasSuccessful()) {
+            return $version_task;
+        }
+
+        // 3. Get the current version from tag
+        $tag = trim($version_task->getMessage());
+        $version = Version::parse($tag);
+        $io->say('Current version: ' . (string) $version);
+
+        // 4. Get bumped version
+        $new_version = clone $version;
+        switch ($type) {
+            case 'major': $new_version->incrementMajor(); break;
+            case 'minor': $new_version->incrementMinor(); break;
+            case 'patch': $new_version->incrementPatch(); break;
+            case 'pre-release': $new_version->incrementPrerelease(); break;
+        }
+        $io->say('New version: ' . (string) $new_version);
+
+        $col = $this->collectionBuilder($io);
+        if ($opts['dry-run']) $col->simulated();
+
+        // 5. Update changelog
+        if ($opts['update-changelog']) {
+            $col->taskReplaceInFile($opts['changelog'])
+                ->regex('/^## \[unreleased\]/mi')
+                ->to('## [' . (string) $new_version . ']')
+                ->limit(1);
+            $col->taskReplaceInFile($opts['changelog'])
+                ->regex('/^\[unreleased\]:(\s?(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:\/?#[\]@!$&\'()*+,;=.]+)\.\.\.HEAD/mi')
+                ->to('[' . (string) $new_version . ']:$1...' . $new_version->prefix($opts['prefix']));
+        }
+
+        // 6. Add and commit CHANGELOG.md
+        $git = $col->taskGitStack();
+
+        if ($opts['update-changelog']) {
+            $git->add($opts['changelog']);
+            $git->commit('Version ' . (string) $new_version);
+        }
+        
+        // 7. Tag
+        $git->tag($new_version->prefix($opts['prefix']));
+
+        // 8. Push
+        if ($opts['push']) {
+            if ($opts['update-changelog']) $git->push();
+            $git->push('--tags');
+        }
+
+        return $col->run();
     }
 }
